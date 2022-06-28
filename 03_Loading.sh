5f3c23c9b8f727f17805ed16c064ddd013f9e622
Yoojung Cho <107462469+YoojungCho@users.noreply.github.com>
2022-06-28 17:36:29 +0900

    Update 03_Loading.sh

--- 03_Loading.sh
+++ 03_Loading.sh
@@ -1,14 +1,17 @@
 echo "data now : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
 
-## PLEASE modify the line below to the directory where your raw file sits and remove the '#'
 export Community_data_dir=/home/tigergraph/02_Community/Community_data/
 
-
-#start all TigerGraph services
 gadmin start all
 
 gsql -g Community "run loading job load_job_claim using
 v_claim=\"${Community_data_dir}/claim.csv\""
+
+gsql -g Community "run loading job load_job_specialties-subspecialties using
+v_specialties_subspecialties=\"${Community_data_dir}/specialties-subspecialties.csv\""
+
+gsql -g Community "run loading job load_job_subspecialties-prescriber using
+v_subspecialties_prescriber=\"${Community_data_dir}/subspecialties-prescriber.csv\""
 
 gsql -g Community "run loading job load_job_claim_associated_with_patient using
 v_claim_associated_with_patient=\"${Community_data_dir}/claim associated with patient.csv\""
@@ -21,11 +24,5 @@ v_flickrEdges=\"${Community_data_dir}/flickrEdges.csv\""
 
 gsql -g Community "run loading job load_job_moreno_kangaroo using
 v_moreno_kangaroo=\"${Community_data_dir}/moreno_kangaroo.csv\""
-
-gsql -g Community "run loading job load_job_specialties-subspecialties using
-v_specialties_subspecialties=\"${Community_data_dir}/specialties-subspecialties.csv\""
-
-gsql -g Community "run loading job load_job_subspecialties-prescriber using
-v_subspecialties_prescriber=\"${Community_data_dir}/subspecialties-prescriber.csv\""
 
 echo "data now : $(date +%Y)-$(date +%m)-$(date +%d) $(date +%H):$(date +%M):$(date +%S)"
